6a4f14e7ee7a8de596a461f912cdeb91a3c8f258
Adamtanacs2002 <WN1KKP@inf.elte.hu>
2024-10-10 19:38:08 +0200

    Hello world project created

---
+++ demos/hello-world/hello-world/hello-world/hello-world.cpp
@@ -0,0 +1,13 @@
+﻿// hello-world.cpp : Defines the entry point for the application.
+//
+
+#include "hello-world.h"
+
+using namespace std;
+using namespace Microsoft::WRL;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ demos/hello-world/hello-world/hello-world/hello-world.h
@@ -0,0 +1,29 @@
+﻿// hello-world.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h> // For CommandLineToArgvW
+
+// The min/max macros conflict with like-named member functions.
+// Only use std::min and std::max defined in <algorithm>.
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+// In order to define a function called CreateWindow, the Windows macro needs to
+// be undefined.
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
